b69ca07796b2d3545cb10e177a2db9e5e8ddb5f4
Hong-JL <56947745+Hong-JL@users.noreply.github.com>
2025-02-20 14:35:59 +0800

    Update config.js

--- config.example.js
+++ config.example.js
@@ -1,7 +1,7 @@
 // 这是一个配置文件模板，复制为config.js后填入你的信息
 const config = {
-    owner: 'your-github-username',
-    repo: 'your-repo-name',
+    owner: 'Hong-JL',
+    repo: 'jifenbang',
     path: '积分数据.csv',
     token: 'your-github-token',
     branch: 'main'
